1950fcbfc683a1eb8036ced06b5ccce8f3917f5c
quyyknb96 <35603560+quyyknb96@users.noreply.github.com>
2018-03-28 12:05:14 +0700

    Add files via upload

---
+++ KuritaDAO/src/main/java/ifi/solution/KuritaDaoApplication.java
@@ -0,0 +1,12 @@
+package ifi.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuritaDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuritaDaoApplication.class, args);
+	}
+}
